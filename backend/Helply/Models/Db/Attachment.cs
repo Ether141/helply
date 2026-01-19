15d9116c9fa4a754231bab721c05d9fba98708bb
Ether141 <42118307+Ether141@users.noreply.github.com>
2026-01-19 23:28:46 +0100

    Add Attachment entity to Helply.Models.Db

---
+++ backend/Helply/Models/Db/Attachment.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Helply.Models.Db;
+
+public class Attachment
+{
+    public Guid Id { get; set; }
+    public string FileName { get; set; } = null!;
+    public string RelativePath { get; set; } = null!;
+    public string FileType { get; set; } = null!;
+    public long FileSize { get; set; }
+    public DateTime CreatedAt { get; set; }
+
+    [ForeignKey(nameof(Author))]
+    public Guid AuthorId { get; set; }
+    public User Author { get; set; } = null!;
+
+    [ForeignKey(nameof(Ticket))]
+    public Guid TicketId { get; set; }
+    public Ticket Ticket { get; set; } = null!;
+}
