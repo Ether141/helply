21e10362de96da5ea7f26cfe70969362589471c4
Ether141 <42118307+Ether141@users.noreply.github.com>
2026-01-19 23:32:28 +0100

    Add UserIdProvider for SignalR user identification

---
+++ backend/Helply/SignalR/UserIdProvider.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Security.Claims;
+
+namespace Helply.SignalR;
+
+public class UserIdProvider : IUserIdProvider
+{
+    public string? GetUserId(HubConnectionContext connection)
+    => connection.User?.FindFirst("sub")?.Value
+       ?? connection.User?.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+}
