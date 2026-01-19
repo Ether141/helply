c210c3becf9738138e4422091e9959eeca01ab3d
Ether141 <42118307+Ether141@users.noreply.github.com>
2026-01-19 23:33:13 +0100

    Add NotificationController with endpoints for notifications
    Added NotificationController with:
    - GET /api/notification/all to fetch unread notifications for the authenticated user
    - POST /api/notification/read/{notificationId} to mark notifications as read
    Utilizes dependency injection, authorization, DTOs, and utility methods for user identification and response formatting.

---
+++ backend/Helply/Controllers/NotificationController.cs
@@ -0,0 +1,60 @@
+﻿using Helply.Models.Dto.Notifications;
+using Helply.Notifications;
+using Helply.Repository;
+using Helply.Utilities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Helply.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class NotificationController : ControllerBase
+{
+    private readonly AppDbContext _db;
+
+    public NotificationController(AppDbContext db)
+    {
+        _db = db;
+    }
+
+    [Authorize]
+    [HttpGet("all")]
+    public async Task<ActionResult<IEnumerable<NotificationResponse>>> GetAll()
+    {
+        var notifications = _db.Notifications
+            .AsNoTrackingWithIdentityResolution()
+            .Include(n => n.Ticket)
+            .Where(n => !n.IsRead && n.UserId == User.GetUserId())
+            .Select(n => new NotificationResponse
+            {
+                NotificationId = n.Id,
+                TicketId = n.TicketId,
+                TicketSlug = n.Ticket.Slug,
+                Message = NotificationsMessageProvider.GetNotificationMessage(n.Type, n.Ticket),
+                CreatedAt = n.CreatedAt
+            });
+
+        return Ok(notifications);
+    }
+
+    [Authorize]
+    [HttpPost("read/{notificationId}")]
+    public async Task<ActionResult> MarkAsRead(string notificationId)
+    {
+        if (!Guid.TryParse(notificationId, out var id))
+        {
+            return BadRequest("Invalid notification ID format.");
+        }
+
+        var notification = await _db.Notifications.FirstOrDefaultAsync(n => n.Id == id);
+
+        if (notification == null) return NotFound();
+
+        notification.IsRead = true;
+
+        await _db.SaveChangesAsync();
+        return NoContent();
+    }
+}
