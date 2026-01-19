a737222e1d890c2ab2b2887633a75a83b250e840
Ether141 <42118307+Ether141@users.noreply.github.com>
2026-01-19 23:30:31 +0100

    Add Notification entity and NotificationType enum

---
+++ backend/Helply/Models/Db/Notification.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Helply.Models.Db;
+
+public class Notification
+{
+    public Guid Id { get; set; }
+    public NotificationType Type { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public bool IsRead { get; set; }
+
+    [ForeignKey(nameof(Ticket))]
+    public Guid TicketId { get; set; }
+    public Ticket Ticket { get; set; } = null!;
+
+    [ForeignKey(nameof(User))]
+    public Guid UserId { get; set; }
+    public User User { get; set; } = null!;
+}
---
+++ backend/Helply/Models/Db/NotificationType.cs
@@ -0,0 +1,8 @@
+﻿namespace Helply.Models.Db;
+
+public enum NotificationType
+{
+    TicketStatusChanged = 0,
+    NewTicketComment = 1,
+    TicketAssigned = 2
+}
