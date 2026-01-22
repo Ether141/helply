7338ac30e6feaf9dcd2c7eac6ab9997b8a78d695
Ether141 <42118307+Ether141@users.noreply.github.com>
2026-01-22 17:01:51 +0100

    Add non-null default to LastName in UserInfoResponse

--- backend/Helply/Models/Dto/Auth/UserInfoResponse.cs
+++ backend/Helply/Models/Dto/Auth/UserInfoResponse.cs
@@ -3,7 +3,7 @@
 public class UserInfoResponse
 {
     public string FirstName { get; set; } = null!;
-    public string LastName { get; set; }
+    public string LastName { get; set; } = null!;
     public string Email { get; set; } = null!;
     public string Role { get; set; } = null!;
 }
